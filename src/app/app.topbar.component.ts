861c91e83cc9ada5854ee40b5964ec4aa7844593
Quirin Manz <qu.manz@tum.de>
2020-11-15 16:07:45 +0100

    add better description text and more consistent style

--- src/app/app.topbar.component.ts
+++ src/app/app.topbar.component.ts
@@ -1,10 +1,10 @@
-import { Component, Inject, forwardRef } from '@angular/core';
-import { AppComponent } from './app.component';
-import { SelectedData } from './service/selected-data';
+import {Component, forwardRef, Inject} from '@angular/core';
+import {AppComponent} from './app.component';
+import {SelectedData} from './service/selected-data';
 
 @Component({
-  selector: 'app-topbar',
-  template: `
+    selector: 'app-topbar',
+    template: `
         <div class="topbar clearfix">
             <div class="topbar-left">
                 <div class="logo"></div>
@@ -20,25 +20,35 @@ import { SelectedData } from './service/selected-data';
                 </a>
 
                 <ul class="topbar-items animated fadeInDown" [ngClass]="{'topbar-items-visible': app.topbarMenuActive}">
-                    <li #settings [ngClass]="{'active-top-menu':app.activeTopbarItem === settings}">
-                      <a href="#" (click)="app.onTopbarItemClick($event,settings)">
-                        <i class="topbar-icon material-icons" style="font-size:44px;">share</i>
-                          <span class="topbar-item-name">Settings</span>
-                      </a>
-                      <ul class="ultima-menu animated fadeInDown" style="width:475px">
-                        <li role="menuitem">
-                          <a>
-                            <i class="material-icons">share</i>
-                            <span>{{ exportLink() }}</span><cmp-copy [text]=exportLink()></cmp-copy>
-                          </a>
-                        </li>
-                      </ul>
+                    <li #settings [ngClass]="{'active-top-menu':app.activeTopbarItem === settings} ">
+                        <a href="#" (click)="app.onTopbarItemClick($event,settings)">
+                            <div>
+                                <p-toolbar>
+                                    <div class="ui-toolbar-group-left" style="margin-right: 2pt;">
+                                        Share Analysis
+                                    </div>
+                                    <i class="topbar-icon material-icons" style="font-size:15px;">share</i>
+                                </p-toolbar>
+                            </div>
+                        </a>
+                        <ul class="ultima-menu animated fadeInDown" style="width:475px">
+                            <li role="menuitem">
+                                <a>
+                                    <i class="material-icons">share</i>
+                                    <span style="margin: 5pt;">{{ exportLink() }}</span>
+                                    <cmp-copy [text]=exportLink()></cmp-copy>
+                                    <div>Once you have completed the data selection wizard you can share your analysis.</div>
+                                </a>
+                            </li>
+                        </ul>
                     </li>
 
-                    <li style="width:475px">
+                    <li style="width:381px">
                         <p-toolbar>
                             <div class="ui-toolbar-group-left">
-                                <label>Dive is in test. Please, give your <a target="_blank" style="color: yellow" href="https://deepblue.userecho.com/">click here for feedback and suggestions</a></label>
+                                <label>Dive is in test. Please, give your <a target="_blank" style="color: yellow"
+                                                                             href="https://deepblue.userecho.com/">feedback
+                                    and suggestions</a></label>
                             </div>
                         </p-toolbar>
                     </li>
@@ -48,12 +58,13 @@ import { SelectedData } from './service/selected-data';
     `
 })
 export class AppTopBar {
-  constructor(@Inject(forwardRef(() => AppComponent)) public app: AppComponent,
-    public selectedData: SelectedData) { }
+    constructor(@Inject(forwardRef(() => AppComponent)) public app: AppComponent,
+                public selectedData: SelectedData) {
+    }
 
-  display = false;
+    display = false;
 
-  exportLink() {
-    return this.selectedData.generateStateUrl();
-  }
+    exportLink() {
+        return this.selectedData.generateStateUrl();
+    }
 }
